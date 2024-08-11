31177cfe6fa948578b211faed6ad525a3c7995b8
arun809097 <163544090+arun809097@users.noreply.github.com>
2024-08-11 10:21:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const http = require("http");
 const express = require("express");
 const app = express();
 const fs = require('fs');
@@ -17,12 +17,12 @@ const serverPort = process.env.PORT || 3000;
 const credentials = { key: privateKey, cert: certificate };
 
 // Create HTTPS server using the SSL certificate files and attach the Express app
-const httpsServer = https.createServer(credentials, app);
+const httpServer = http.createServer(credentials, app);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const server = httpsServer.listen(serverPort, () => {
+const server = httpServer.listen(serverPort, () => {
     console.log(`Server listening on port ${serverPort}`);
 });
