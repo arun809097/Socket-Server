85995f09c8b98fde1c64260996eb746b912ff32b
arun809097 <163544090+arun809097@users.noreply.github.com>
2024-08-11 09:54:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,108 +1,49 @@
-const IO = require('socket.io-client');
 const fs = require('fs');
-const https = require('https');
-const socketIO = require('socket.io');
-// Read the SSL certificate files
+const IO = require('socket.io-client');
 const privateKey = fs.readFileSync('./private.key', 'utf8');
 const certificate = fs.readFileSync('./certificate.cert', 'utf8');
 
-// Create HTTPS server using the SSL certificate files
-const credentials = { key: privateKey, cert: certificate };
-const httpsServer = https.createServer(credentials);
- 
- 
-const port = 3000;
- 
-
-const ConnectBase=(_io,_type,_id)=>{
-    
-    const socket = IO('wss://spusher.jpl99.in', {transports: [ 'websocket' ], reconnection: true,
-reconnectionDelay: 1000,
-reconnectionDelayMax : 5000,
-reconnectionAttempts: 99999,
-  extraHeaders: {
-    Origin: 'https://balaji12.co' // Replace with your desired custom origin
-  }
-});
-
-socket.on('connect', () => {
- console.log('connected...');
-  socket.emit(_type,  _id);
-
-});
-  
-socket.on('alwarevents/'+_id, (data) => {
-    _io.emit('gems/'+_id, data);
-  const market_id= JSON.parse(data).market_id;
-    socket.emit('bm_odds', market_id);
-    
-    socket.emit('auto_fancy', market_id);
-});
-
- socket.on('casino/'+_id, (data) => {
-  _io.emit('gems/'+_id, data);
-  
+try {
+    const socket = IO('wss://eventv4.urbet.in', {
+        transports: ['websocket'],
+        reconnection: true,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        reconnectionAttempts: 99999,
+        extraHeaders: {
+            Origin: 'https://urbet.in' // Replace with your desired custom origin
+        },
+        // SSL/TLS options
+        key: privateKey,
+        cert: certificate 
     });
 
-   
-socket.on('bm_odds',(...data) => {
-  
-  _io.emit('bm_odds', data);
+    socket.on('connect', () => {
+        console.log('Connected to WebSocket');
+        socket.emit('casino', 'abj');
+    });
 
-});
-   
-socket.on('autofancy',(...data) => {
-  
-  _io.emit('autofancy', data);
+    socket.on('disconnect', () => {
+        console.log('WebSocket connection closed');
+    });
 
-});
-   
-    
-    
-  
+    socket.on('error', (error) => {
+        console.error('WebSocket connection error:', error);
+    });
 
-socket.on('disconnect', () => {
-  console.log('WebSocket connection closed');
-});
+    socket.on('connect_error', (error) => {
+        console.error('Connection error:', error.message);
+    });
 
-    
+} catch (err) {
+    console.error('Synchronous error caught:', err);
 }
 
-// Start the Express server
-const server = httpsServer.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
 
-// Create an instance of Socket.IO and listen on the server
-const io = socketIO(httpsServer);
 
-// Handle Socket.IO connection
-io.on('connection', (_io) => {
-  console.log('A client connected.');
- 
-   _io.on('casino',(ID) => {
-    
-  ConnectBase(_io,'casino',ID);
-  
-  //  ConnectBase(_socket,event.replace('gems','alwarevents'),args);
- 
-  //  socket.emit(event.replace('gems','alwarevents'), args);
- 
-   });
-  _io.on('gems',(ID) => {
-   
-  ConnectBase(_io,'alwarevents',ID);
-  
-  //  ConnectBase(_socket,event.replace('gems','alwarevents'),args);
- 
-  //  socket.emit(event.replace('gems','alwarevents'), args);
- 
-   });
-  
-  // Handle the 'disconnect' event
- // socket.on('disconnect', () => {
-  //  console.log('A client disconnected.');
-  //});
-});
- 
+
+
+
+
+
  
