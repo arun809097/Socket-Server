79a0f6386fc4f824e34d7c6b0c98f2c6d4cf41b0
arun809097 <163544090+arun809097@users.noreply.github.com>
2024-08-11 10:08:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,55 @@
 const https = require("https");
 const fs = require('fs');
 const express = require("express");
+const IO = require('socket.io-client');
 const app = express();
-app.use(express.static("public"));
+
+// Uncomment if using environment variables
 // require("dotenv").config();
 
-const IO = require('socket.io-client');
+app.use(express.static("public"));
 
 const privateKey = fs.readFileSync('./private.key', 'utf8');
 const certificate = fs.readFileSync('./certificate.cert', 'utf8');
 
 const serverPort = process.env.PORT || 3000;
-// Create HTTPS server using the SSL certificate files
 const credentials = { key: privateKey, cert: certificate };
-const httpsServer = https.createServer(credentials);
- 
-const server = httpsServer.listen(serverPort, () => {
+
+// Create HTTPS server and attach the Express app
+const httpsServer = https.createServer(credentials, app);
+
+// Start the server
+httpsServer.listen(serverPort, () => {
     console.log(`Server listening on port ${serverPort}`);
-  });
-  
+});
 
-   
-
-try {
-    const socket = IO('wss://eventv4.urbet.in', {
-      transports: ['websocket'],
-      reconnection: true,
-      reconnectionDelay: 1000,
-      reconnectionDelayMax: 5000,
-      reconnectionAttempts: 99999,
-      extraHeaders: {
-        Origin: 'https://urbet.in' // Replace with your desired custom origin
-      }
-    });
-  
-    socket.on('connect', () => {
-      console.log('Connected to WebSocket');
-      socket.emit('casino', 'abj');
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('WebSocket connection closed');
-    });
-  
-    socket.on('error', (error) => {
-      console.error('WebSocket connection error:', error);
-    });
-  
-    socket.on('connect_error', (error) => {
-      console.error('Connection error:', error.message);
-    });
-  
-  } catch (err) {
-    console.error('Synchronous error caught:', err);
+const socket = IO('wss://eventv4.urbet.in', {
+  transports: ['websocket'],
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  reconnectionAttempts: 99999,
+  extraHeaders: {
+    Origin: 'https://urbet.in' // Replace with your desired custom origin
   }
+});
 
+socket.on('connect', () => {
+  console.log('Connected to WebSocket');
+  socket.emit('casino', 'abj');
+});
 
+socket.on('disconnect', () => {
+  console.log('WebSocket connection closed');
+});
+
+socket.on('error', (error) => {
+  console.error('WebSocket connection error:', error);
+});
+
+socket.on('connect_error', (error) => {
+  console.error('Connection error:', error.message);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
