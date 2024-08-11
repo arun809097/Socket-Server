fb593bd57dfae1c6d792d7ac179503b7213bc388
arun809097 <163544090+arun809097@users.noreply.github.com>
2024-08-11 10:30:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-const fs = require('fs');
 
 // Uncomment if using environment variables
 // require("dotenv").config();
@@ -10,14 +9,11 @@ app.use(express.static("public"));
 
 const IO = require('socket.io-client');
 
-const privateKey = fs.readFileSync('./private.key', 'utf8');
-const certificate = fs.readFileSync('./certificate.cert', 'utf8');
 
 const serverPort = process.env.PORT || 3000;
-const credentials = { key: privateKey, cert: certificate };
 
 // Create HTTPS server using the SSL certificate files and attach the Express app
-const httpServer = http.createServer(credentials, app);
+const httpServer = http.createServer( app);
 
 app.get('/', (req, res) => {
   
