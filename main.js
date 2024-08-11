a305285cf8276fe9909ce2c7aa43a5b3c2fb6e03
arun809097 <163544090+arun809097@users.noreply.github.com>
2024-08-11 09:40:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,88 +1,108 @@
-const https = require("https");
-const express = require("express");
-const app = express();
 const IO = require('socket.io-client');
-app.use(express.static("public"));
-// require("dotenv").config();
+const fs = require('fs');
+const https = require('https');
+const socket = require('socket.io');
+// Read the SSL certificate files
+const privateKey = fs.readFileSync('./private.key', 'utf8');
+const certificate = fs.readFileSync('./certificate.cert', 'utf8');
 
-
-
-const serverPort = process.env.PORT || 3002;
-const server = https.createServer(app);
-const WebSocket = require("ws");
-
-let keepAliveId;
-
-const wss =
-  process.env.NODE_ENV === "production"
-    ? new WebSocket.Server({ server })
-    : new WebSocket.Server({ port: 5001 });
-
-server.listen(serverPort);
-
-console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_ENV}`);
-
-wss.on("connection", function (ws, req) {
-  console.log("Connection Opened");
-  console.log("Client size: ", wss.clients.size);
-
-  if (wss.clients.size === 1) {
-    console.log("first connection. starting keepalive");
-    keepServerAlive();
-  }
+// Create HTTPS server using the SSL certificate files
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials);
  
+ 
+const port = 9792;
+ 
+
+const ConnectBase=(_io,_type,_id)=>{
     
-  ws.on("message", (data) => {
-    let stringifiedData = data.toString();
-    if (stringifiedData === 'pong') {
-      console.log('keepAlive');
-      return;
-    }  
-   
-   
-  });
-
-  ws.on("close", (data) => {
-    console.log("closing connection");
-
-    if (wss.clients.size === 0) {
-      console.log("last client disconnected, stopping keepAlive interval");
-      clearInterval(keepAliveId);
-    }
-  });
-});
-
-// Implement broadcast function because of ws doesn't have it
-const broadcast = (ws, message, includeSelf) => {
-  if (includeSelf) {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  } else {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+    const socket = IO('wss://spusher.jpl99.in', {transports: [ 'websocket' ], reconnection: true,
+reconnectionDelay: 1000,
+reconnectionDelayMax : 5000,
+reconnectionAttempts: 99999,
+  extraHeaders: {
+    Origin: 'https://balaji12.co' // Replace with your desired custom origin
   }
-};
-
-/**
- * Sends a ping message to all connected clients every 50 seconds
- */
- const keepServerAlive = () => {
-  keepAliveId = setInterval(() => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
-      }
-    });
-  }, 50000);
-};
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
 });
+
+socket.on('connect', () => {
+ console.log('connected...');
+  socket.emit(_type,  _id);
+
+});
+  
+socket.on('alwarevents/'+_id, (data) => {
+    _io.emit('gems/'+_id, data);
+  const market_id= JSON.parse(data).market_id;
+    socket.emit('bm_odds', market_id);
+    
+    socket.emit('auto_fancy', market_id);
+});
+
+ socket.on('casino/'+_id, (data) => {
+  _io.emit('gems/'+_id, data);
+  
+    });
+
+   
+socket.on('bm_odds',(...data) => {
+  
+  _io.emit('bm_odds', data);
+
+});
+   
+socket.on('autofancy',(...data) => {
+  
+  _io.emit('autofancy', data);
+
+});
+   
+    
+    
+  
+
+socket.on('disconnect', () => {
+  console.log('WebSocket connection closed');
+});
+
+    
+}
+
+// Start the Express server
+const server = httpsServer.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Create an instance of Socket.IO and listen on the server
+const io = socketIO(httpsServer);
+
+// Handle Socket.IO connection
+io.on('connection', (_io) => {
+  console.log('A client connected.');
+ 
+   _io.on('casino',(ID) => {
+    
+  ConnectBase(_io,'casino',ID);
+  
+  //  ConnectBase(_socket,event.replace('gems','alwarevents'),args);
+ 
+  //  socket.emit(event.replace('gems','alwarevents'), args);
+ 
+   });
+  _io.on('gems',(ID) => {
+   
+  ConnectBase(_io,'alwarevents',ID);
+  
+  //  ConnectBase(_socket,event.replace('gems','alwarevents'),args);
+ 
+  //  socket.emit(event.replace('gems','alwarevents'), args);
+ 
+   });
+  
+  // Handle the 'disconnect' event
+ // socket.on('disconnect', () => {
+  //  console.log('A client disconnected.');
+  //});
+});
+ 
+ 
