6811bc780733fab2530d61a17f384f74c9b7c2d7
arun809097 <163544090+arun809097@users.noreply.github.com>
2024-08-11 10:22:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,40 @@ const credentials = { key: privateKey, cert: certificate };
 const httpServer = http.createServer(credentials, app);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  
+
+try {
+    const socket = IO('wss://eventv4.urbet.in', {
+      transports: ['websocket'],
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+      reconnectionAttempts: 99999,
+      extraHeaders: {
+        Origin: 'https://urbet.in' // Replace with your desired custom origin
+      }
+    });
+  
+    socket.on('connect', () => {
+      console.log('Connected to WebSocket');
+      socket.emit('casino', 'abj');
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('WebSocket connection closed');
+    });
+  
+    socket.on('error', (error) => {
+      console.error('WebSocket connection error:', error);
+    });
+  
+    socket.on('connect_error', (error) => {
+      console.error('Connection error:', error.message);
+    });
+  
+  } catch (err) {
+    console.error('Synchronous error caught:', err);
+  }
 });
 
 const server = httpServer.listen(serverPort, () => {
